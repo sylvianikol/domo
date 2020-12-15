33f17c51d61e7e7a83ec225bcbab41d2312ffbea
sylvianikol <silviaadvert@gmail.com>
2020-12-15 09:25:32 +0200

    refactoring

--- src/main/java/com/syn/domo/service/FloorService.java
+++ src/main/java/com/syn/domo/service/FloorService.java
@@ -1,10 +1,8 @@
 package com.syn.domo.service;
 
-import com.syn.domo.model.entity.Floor;
 import com.syn.domo.model.service.FloorServiceModel;
 
 import java.util.List;
-import java.util.Set;
 
 public interface FloorService {
 
@@ -12,11 +10,5 @@ public interface FloorService {
 
     void createFloors(int floorsNumber, String buildingId);
 
-    Set<Floor> getAllByBuildingId(Long id);
-
-    boolean hasFloors();
-
     List<Integer> getAllFloorNumbers();
-
-    int countFloors();
 }

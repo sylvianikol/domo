3114401e557d86e2b1119ef0c2d8d05d5eea980c
sylvianikol <silviaadvert@gmail.com>
2020-12-28 09:48:08 +0200

    raised abstraction for ResidentEditBindingModel

--- src/main/java/com/syn/domo/model/binding/ChildAddBindingModel.java
+++ src/main/java/com/syn/domo/model/binding/ChildAddBindingModel.java
@@ -1,10 +1,37 @@
 package com.syn.domo.model.binding;
 
-public class ChildAddBindingModel extends BaseUserEntityBindingModel {
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
+import static com.syn.domo.common.ValidationErrorMessages.*;
+
+public class ChildAddBindingModel {
+
+    private String firstName;
+    private String lastName;
     private String apartmentId;
 
     public ChildAddBindingModel() {
+    }
+
+    @NotNull(message = FIRST_NAME_NULL)
+    @NotEmpty(message = FIRST_NAME_EMPTY)
+    @Size(min = 2, max = 55, message = FIRST_NAME_INVALID)
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public String getApartmentId() {
--- src/main/java/com/syn/domo/model/binding/ResidentEditBindingModel.java
+++ src/main/java/com/syn/domo/model/binding/ResidentEditBindingModel.java
@@ -3,16 +3,11 @@ package com.syn.domo.model.binding;
 import javax.validation.constraints.*;
 import java.time.LocalDate;
 
-import static com.syn.domo.common.RegexPatterns.PHONE_NUMBER_REGEX;
 import static com.syn.domo.common.ValidationErrorMessages.*;
 
-public class ResidentEditBindingModel {
+public class ResidentEditBindingModel extends UserEntityBindingModel{
 
     private String id;
-    private String firstName;
-    private String lastName;
-    private String email;
-    private String phoneNumber;
     private String userRole;
     private LocalDate addedOn;
 
@@ -27,51 +22,6 @@ public class ResidentEditBindingModel {
 
     public void setId(String id) {
         this.id = id;
-    }
-
-    @NotNull(message = FIRST_NAME_NULL)
-    @NotEmpty(message = FIRST_NAME_EMPTY)
-    @Size(min = 2, max = 55, message = FIRST_NAME_INVALID)
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    @NotNull(message = LAST_NAME_NULL)
-    @NotEmpty(message = LAST_NAME_EMPTY)
-    @Size(min = 2, max = 55, message = LAST_NAME_INVALID)
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    @NotNull(message = EMAIL_NULL)
-    @NotEmpty(message = EMAIL_EMPTY)
-    @Email(message = EMAIL_INVALID)
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @NotNull(message = PHONE_NULL)
-    @NotEmpty(message = PHONE_EMPTY)
-    @Pattern(regexp = PHONE_NUMBER_REGEX, message = PHONE_INVALID)
-    @Size(max = 20, message = PHONE_LENGTH)
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
     }
 
     @NotNull(message = ROLE_NULL)

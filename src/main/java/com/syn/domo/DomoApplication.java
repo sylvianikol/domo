b8abbfe78ef7606f9490ec764c07e50269d9eb94
sylvianikol <silviaadvert@gmail.com>
2021-01-06 11:38:47 +0200

    @EnableScheduling

--- src/main/java/com/syn/domo/DomoApplication.java
+++ src/main/java/com/syn/domo/DomoApplication.java
@@ -2,8 +2,10 @@ package com.syn.domo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class DomoApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/syn/domo/model/entity/Fee.java
@@ -0,0 +1,65 @@
+package com.syn.domo.model.entity;
+
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.Column;
+import javax.persistence.ManyToOne;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Fee extends BaseEntity {
+
+    private BigDecimal total;
+    private LocalDate issueDate;
+    private LocalDate dueDate;
+    private boolean isPaid;
+    private Apartment apartment;
+
+    public Fee() {
+    }
+
+    @ColumnDefault("0")
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+
+    @Column(name = "start_date", nullable = false)
+    public LocalDate getIssueDate() {
+        return issueDate;
+    }
+
+    public void setIssueDate(LocalDate issueDate) {
+        this.issueDate = issueDate;
+    }
+
+    @Column(name = "due_date")
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    @Column(name = "paid")
+    public boolean isPaid() {
+        return isPaid;
+    }
+
+    public void setPaid(boolean paid) {
+        isPaid = paid;
+    }
+
+    @ManyToOne
+    public Apartment getApartment() {
+        return apartment;
+    }
+
+    public void setApartment(Apartment apartment) {
+        this.apartment = apartment;
+    }
+}

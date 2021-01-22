014e34fdf2ea6e26fd692d20ce46784e8184fd95
sylvianikol <silviaadvert@gmail.com>
2021-01-22 12:22:34 +0200

    added RoleRepositoryTest

--- src/test/java/com/syn/domo/repository/RoleRepositoryTest.java
+++ src/test/java/com/syn/domo/repository/RoleRepositoryTest.java
@@ -11,7 +11,6 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 class RoleRepositoryTest {

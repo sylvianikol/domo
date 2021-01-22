9b4d7d1242bcd741ec86159e47ee374013257169
sylvianikol <silviaadvert@gmail.com>
2021-01-22 13:05:42 +0200

    refactoring

--- src/test/java/com/syn/domo/repository/ApartmentRepositoryTest.java
+++ src/test/java/com/syn/domo/repository/ApartmentRepositoryTest.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 class ApartmentRepositoryTest {
@@ -23,7 +24,10 @@ class ApartmentRepositoryTest {
     @Autowired
     private BuildingRepository buildingRepository;
     private Building building;
-    private Apartment apartment;
+
+    private String BUILDING_ID;
+    private String APARTMENT_ID;
+    private String APARTMENT_NUMBER;
 
     @BeforeEach
     public void setUp() {
@@ -32,10 +36,14 @@ class ApartmentRepositoryTest {
         this.building = new Building("TestBuilding 1",
                 "Test neighbourhood", "TestAddress",3,
                 BigDecimal.valueOf(5), BigDecimal.valueOf(100), LocalDate.now());
-        this.buildingRepository.saveAndFlush(building);
+        this.buildingRepository.saveAndFlush(this.building);
 
-        apartment = new Apartment("1", 1, building, 0, LocalDate.now());
+        Apartment apartment = new Apartment("1", 1, building, 0, LocalDate.now());
         this.apartmentRepository.saveAndFlush(apartment);
+
+        BUILDING_ID = this.building.getId();
+        APARTMENT_ID = apartment.getId();
+        APARTMENT_NUMBER = apartment.getNumber();
     }
 
     @AfterEach
@@ -48,29 +56,27 @@ class ApartmentRepositoryTest {
     void injectedComponentsAreNotNull() {
         assertThat(this.apartmentRepository).isNotNull();
         assertThat(this.buildingRepository).isNotNull();
-        assertThat(this.building).isNotNull();
-        assertThat(this.apartment).isNotNull();
     }
 
     @Test
     void test_findByNumberAndBuildingId_IsPresent() {
 
         Optional<Apartment> found = this.apartmentRepository
-                .findByNumberAndBuildingId(apartment.getNumber(), building.getId());
+                .findByNumberAndBuildingId(APARTMENT_NUMBER, BUILDING_ID);
 
         assertThat(found).isPresent();
-        assertThat(found.get().getId().equals(this.apartment.getId()));
+        assertEquals(found.get().getId(), APARTMENT_ID);
     }
 
     @Test
     void test_findByNumberAndBuildingId_IsEmpty() {
         Optional<Apartment> found = this.apartmentRepository
-                .findByNumberAndBuildingId("2", building.getId());
+                .findByNumberAndBuildingId("2", BUILDING_ID);
 
         assertThat(found).isEmpty();
 
         found = this.apartmentRepository
-                .findByNumberAndBuildingId(apartment.getNumber(), "2");
+                .findByNumberAndBuildingId(APARTMENT_NUMBER, "2");
 
         assertThat(found).isEmpty();
     }
@@ -78,11 +84,11 @@ class ApartmentRepositoryTest {
     @Test
     void test_getDuplicateApartment_IsPresent() {
 
-        Apartment apartment2 = new Apartment("1", 1, building, 0, LocalDate.now());
+        Apartment apartment2 = new Apartment("1", 1, this.building, 0, LocalDate.now());
         this.apartmentRepository.saveAndFlush(apartment2);
 
         Optional<Apartment> duplicate = this.apartmentRepository
-                .getDuplicateApartment(apartment2.getNumber(), building.getId(), apartment2.getId());
+                .getDuplicateApartment(apartment2.getNumber(), BUILDING_ID, apartment2.getId());
 
         assertThat(duplicate).isPresent();
     }
@@ -90,11 +96,11 @@ class ApartmentRepositoryTest {
     @Test
     void test_getDuplicateApartment_IsEmpty() {
 
-        Apartment apartment2 = new Apartment("2", 1, building, 0, LocalDate.now());
+        Apartment apartment2 = new Apartment("2", 1, this.building, 0, LocalDate.now());
         this.apartmentRepository.saveAndFlush(apartment2);
 
         Optional<Apartment> duplicate = this.apartmentRepository
-                .getDuplicateApartment(apartment2.getNumber(), building.getId(), apartment2.getId());
+                .getDuplicateApartment(apartment2.getNumber(), BUILDING_ID, apartment2.getId());
 
         assertThat(duplicate).isEmpty();
     }
@@ -102,25 +108,26 @@ class ApartmentRepositoryTest {
     @Test
     void test_getDuplicateApartment_returnsDuplicate() {
 
-        Apartment apartment2 = new Apartment("1", 1, building, 0, LocalDate.now());
+        Apartment apartment2 = new Apartment("1", 1, this.building, 0, LocalDate.now());
         this.apartmentRepository.saveAndFlush(apartment2);
 
         Apartment duplicate = this.apartmentRepository
-                .getDuplicateApartment("1", building.getId(), apartment2.getId())
+                .getDuplicateApartment("1", BUILDING_ID, apartment2.getId())
                 .orElse(null);
 
         assertThat(duplicate).isNotNull();
-        assertThat(duplicate.getNumber().equals("1"));
-        assertThat(duplicate.getBuilding().getId().equals(building.getId()));
-        assertThat(!duplicate.getId().equals(apartment2.getId()));
+        assertEquals(duplicate.getNumber(), "1");
+        assertEquals(duplicate.getBuilding().getId(), BUILDING_ID);
+        assertNotEquals(duplicate.getId(), apartment2.getId());
     }
 
     @Test
     void test_getAllByBuildingId_isNotEmpty() {
         Set<Apartment> apartments = this.apartmentRepository
-                .findAllByBuildingId(building.getId());
+                .findAllByBuildingId(BUILDING_ID);
 
         assertThat(apartments).isNotEmpty();
+        assertEquals(apartments.iterator().next().getBuilding().getId(), BUILDING_ID);
     }
 
     @Test
@@ -134,28 +141,30 @@ class ApartmentRepositoryTest {
     @Test
     void test_getAllByBuildingId_returnsExpectedSize() {
         Set<Apartment> apartments = this.apartmentRepository
-                .findAllByBuildingId(this.building.getId());
+                .findAllByBuildingId(BUILDING_ID);
 
-        assertThat(apartments.size() == 1);
+        assertEquals(apartments.size(), 1);
     }
 
     @Test
     void test_findByIdAndBuildingId_IsPresent() {
         Optional<Apartment> found = this.apartmentRepository
-                .findByIdAndBuildingId(apartment.getId(), building.getId());
+                .findByIdAndBuildingId(APARTMENT_ID, BUILDING_ID);
 
         assertThat(found).isPresent();
+        assertEquals(found.get().getId(), APARTMENT_ID);
+        assertEquals(found.get().getBuilding().getId(), BUILDING_ID);
     }
 
     @Test
     void test_findByIdAndBuildingId_IsEmpty() {
         Optional<Apartment> found = this.apartmentRepository
-                .findByIdAndBuildingId("0", building.getId());
+                .findByIdAndBuildingId("0", BUILDING_ID);
 
         assertThat(found).isEmpty();
 
         found = this.apartmentRepository
-                .findByIdAndBuildingId(apartment.getId(), "0");
+                .findByIdAndBuildingId(APARTMENT_ID, "0");
 
         assertThat(found).isEmpty();
 
@@ -163,14 +172,5 @@ class ApartmentRepositoryTest {
                 .findByIdAndBuildingId("0", "0");
 
         assertThat(found).isEmpty();
-    }
-
-    @Test
-    void test_findByIdAndBuildingId_returnsCorrectApartment() {
-        Optional<Apartment> found = this.apartmentRepository
-                .findByIdAndBuildingId(apartment.getId(), building.getId());
-
-        assertThat(found).isPresent();
-        assertThat(found.get().getId().equals(this.apartment.getId()));
     }
 }
